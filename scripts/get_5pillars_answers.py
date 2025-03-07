19652335557e4578d0e27bb44a9557c871a4c091
Zhenghua Bao <zheng.hua.b@gmail.com>
2025-03-07 15:24:33 +0000

    minor adaptation to get rid of demonstrations

--- scripts/get_5pillars_answers.py
+++ scripts/get_5pillars_answers.py
@@ -7,6 +7,7 @@ sys.path.insert(1, os.path.join(sys.path[0], ".."))
 from baseline.answer_generation import *
 from baseline.generation_utils import *
 from baseline.llm_prompting import *
+from dotenv import load_dotenv
 
 
 # Export your OpenAI API in your environment for later use
@@ -54,7 +55,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--modality",
         type=str,
-        default="vision",
+        default="multimodal",
         help="Which input modality to use. One of [vision, evidence, multimodal]",
     )
     parser.add_argument(
@@ -63,7 +64,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--model",
         type=str,
-        default="llava",
+        default="gpt4",
         help="Which LLM to use for generating answers.",
     )
     parser.add_argument(
@@ -85,7 +86,10 @@ if __name__ == "__main__":
         help="The waiting time between two answer generation.",
     )
 
+    load_dotenv()
+
     args = parser.parse_args()
+
     if args.model == "gpt4":
         client = OpenAI()
     else:
@@ -106,7 +110,7 @@ if __name__ == "__main__":
     # Prepare data
     train = load_json("dataset/train.json")
     # Load test images
-    test = load_json("dataset/test.json")
+    test = load_json("dataset/test_test.json")
     task_test = [t for t in test if t[args.task] != "not enough information"]
     image_paths = [t["image path"] for t in task_test]
     if args.task == "date":
@@ -118,7 +122,7 @@ if __name__ == "__main__":
     clip_evidence_embeddings = np.load("dataset/embeddings/evidence_embeddings.npy")
     image_embeddings = np.load("dataset/embeddings/image_embeddings.npy")
     image_embeddings_map = load_json("dataset/embeddings/image_embeddings_map.json")
-    evidence = load_json("dataset/retrieval_results/evidence.json")
+    evidence = load_json("dataset/retrieval_results/test_evidence.json")
     # Select evidence and demonstrations
     evidence_idx = []
     if args.modality in ["evidence", "multimodal"]:
@@ -134,44 +138,44 @@ if __name__ == "__main__":
             )
     # Select demonstrations
     # Keep train images that have evidence
-    images_with_evidence = [ev["image path"] for ev in evidence]
-    demonstration_candidates = [
-        t for t in train if t["image path"] in images_with_evidence
-    ]
-    demonstrations = []
-    for i in range(len(image_paths)):
-        if args.n_shots > 0:
+    # images_with_evidence = [ev["image path"] for ev in evidence]
+    # demonstration_candidates = [
+    #     t for t in train if t["image path"] in images_with_evidence
+    # ]
+    # demonstrations = []
+    # for i in range(len(image_paths)):
+    #     if args.n_shots > 0:
 
-            demonstrations_idx = get_topk_demonstrations(
-                image_paths[i],
-                args.task,
-                demonstration_candidates,
-                image_embeddings,
-                image_embeddings_map,
-                args.n_shots,
-            )
-            instance_demonstrations = []
-            for idx in demonstrations_idx:
-                # Get the top k evidence for each demonstration
-                demo_image = demonstration_candidates[idx]["image path"]
-                demo_answer = demonstration_candidates[idx][args.task]
-                demo_evidence_idx = get_topk_evidence(
-                    demo_image,
-                    evidence,
-                    image_embeddings,
-                    clip_evidence_embeddings,
-                    image_embeddings_map,
-                )
-                evidence_image_subset = [
-                    ev for ev in evidence if ev["image path"] == demo_image
-                ]
-                demo_evidence = [
-                    evidence_image_subset[idx] for idx in demo_evidence_idx
-                ]
-                instance_demonstrations.append((demo_image, demo_answer, demo_evidence))
-            demonstrations.append(instance_demonstrations)
-        else:
-            demonstrations.append([])
+    #         demonstrations_idx = get_topk_demonstrations(
+    #             image_paths[i],
+    #             args.task,
+    #             demonstration_candidates,
+    #             image_embeddings,
+    #             image_embeddings_map,
+    #             args.n_shots,
+    #         )
+    #         instance_demonstrations = []
+    #         for idx in demonstrations_idx:
+    #             # Get the top k evidence for each demonstration
+    #             demo_image = demonstration_candidates[idx]["image path"]
+    #             demo_answer = demonstration_candidates[idx][args.task]
+    #             demo_evidence_idx = get_topk_evidence(
+    #                 demo_image,
+    #                 evidence,
+    #                 image_embeddings,
+    #                 clip_evidence_embeddings,
+    #                 image_embeddings_map,
+    #             )
+    #             evidence_image_subset = [
+    #                 ev for ev in evidence if ev["image path"] == demo_image
+    #             ]
+    #             demo_evidence = [
+    #                 evidence_image_subset[idx] for idx in demo_evidence_idx
+    #             ]
+    #             instance_demonstrations.append((demo_image, demo_answer, demo_evidence))
+    #         demonstrations.append(instance_demonstrations)
+    #     else:
+    #         demonstrations.append([])
 
     # Run the main loop
     run_model(
@@ -184,7 +188,7 @@ if __name__ == "__main__":
         args.model,
         evidence,
         evidence_idx,
-        demonstrations,
+        [],
         client,
         args.max_tokens,
         args.temperature,
