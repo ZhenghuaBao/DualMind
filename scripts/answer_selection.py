deb1e0efc9b8227a2eae4eabd2a9b406cf8aff44
Zhenghua Bao <zheng.hua.b@gmail.com>
2025-03-10 15:53:14 +0000

    current answer selection pipeline for merging fallback answers

--- scripts/answer_selection.py
+++ scripts/answer_selection.py
@@ -6,16 +6,19 @@ from utils import load_json
 import json
 
 if __name__ == "__main__":
-    core_answers = load_json("output/original_results_location.json")
-    fallback_answers = load_json("output/keyword_results_location.json")
+    core_answers = load_json("output/complete_core_results_source.json")
+    fallback_answers = load_json("output/context_fallback_results_source.json")
 
     final_answers = []
 
     for idx, core_answer in enumerate(core_answers):
-        if core_answer["output"] == "NaN":
+        # Date: [ "I can't", "I'm sorry", "I don't", "I'm unable to"]
+        # Location: ["Unknown location", ""I'm unable to", ""Not specified."]
+        invalid_answers = [ "I can't", "I'm sorry", "I don't", "I'm unable to", "Unknown location", "Not specified."]
+        if any(core_answer["output"].startswith(answer) for answer in invalid_answers):
             final_answers.append(fallback_answers[idx])
         else:
             final_answers.append(core_answer)
     
-    with open("output/final_results_location.json", "w", encoding="utf-8") as file:
+    with open("output/final_results_source.json", "w", encoding="utf-8") as file:
         json.dump(final_answers, file, indent=4)
